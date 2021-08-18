1632909014f956efd3de7418b25df36cd9159610
Frank W. Zammetti <fzammetti@etherient.com>
2021-08-18 14:35:30 -0400

    Adding test classes

--- basic_rest/src/test/java/com/etherient/basic_rest/BasicRestApplicationTests.java
+++ basic_rest/src/test/java/com/etherient/basic_rest/BasicRestApplicationTests.java
@@ -1,13 +1,55 @@
 package com.etherient.basic_rest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * A simple unit test for the TestController.
+ *
+ * @author <a href="mailto:fzammetti@etheriebt.com">Frank W. Zammetti</a>.
+ */
 @SpringBootTest
+@AutoConfigureMockMvc
 class BasicRestApplicationTests {
 
+
+  /**
+   * Mock MVC support.
+   */
+  @Autowired
+  private MockMvc mvc;
+
+
+  /**
+   * Confirm that the context loads properly.
+   */
   @Test
   void contextLoads() {
-  }
 
-}
+    System.out.println("* Testing if context loads");
+
+  } /* End contextLoads(). */
+
+
+  @Test
+  public void testHelloWorld() throws Exception {
+
+    System.out.println("* Testing /helloWorld");
+    mvc.perform(MockMvcRequestBuilders.get("/helloWorld").accept(MediaType.TEXT_PLAIN))
+      .andExpect(status().isOk())
+      .andExpect(content().string(equalTo("Hello, from the TestController!")));
+
+  } /* End testHelloWorld(). */
+
+
+} /* End class. */
--- thymeleaf_example/src/test/java/com/etherient/thymeleaf_example/ThymeleafExampleApplicationTests.java
+++ thymeleaf_example/src/test/java/com/etherient/thymeleaf_example/ThymeleafExampleApplicationTests.java
@@ -1,13 +1,28 @@
 package com.etherient.thymeleaf_example;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ThymeleafExampleApplicationTests {
 
+/**
+ * A simple unit test.
+ *
+ * @author <a href="mailto:fzammetti@etheriebt.com">Frank W. Zammetti</a>.
+ */
+@SpringBootTest
+class BasicRestApplicationTests {
+
+
+  /**
+   * Confirm that the context loads properly.
+   */
   @Test
   void contextLoads() {
-  }
 
-}
+    System.out.println("* Testing if context loads");
+
+  } /* End contextLoads(). */
+
+
+} /* End class. */
